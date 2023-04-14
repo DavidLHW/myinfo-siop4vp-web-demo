535511ea1bdaaca3f8f443fb872936e5984fbb7e
David <david.lin@aurabitcapital.com>
2023-04-14 15:47:02 +0800

    fix: flash of unstyled content

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,29 @@
-import { Html, Head, Main, NextScript } from "next/document";
+/**
+ * This script packages styled-components into SSR, fixes flash-of-unstyled-commponents on initial render
+ * https://styled-components.com/docs/advanced#with-swc
+ */
+import Document, { DocumentContext } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 }
